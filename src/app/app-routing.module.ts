5859206999e6d6d2c0139bfd842380178eeb6d88
AmauryDanisCousandier <amaury.danis-cousandier@epitech.eu>
2022-01-18 19:45:17 +0100

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,10 @@
+/*
+** Amaury PROJECT, 2021
+** VideoGames_MuseumCompanion [WSL: Ubuntu-20.04]
+** File description:
+** app-routing.module.ts
+*/
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
